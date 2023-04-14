e0ec83bd3cf17178b7d63c7dd4d34e7c99f81826
okauppinen <olli.kauppinen@nls.fi>
2023-04-14 10:27:07 +0300

    remove get/setName from userdatalayer

--- service-base/src/main/java/fi/nls/oskari/domain/map/UserDataLayer.java
+++ service-base/src/main/java/fi/nls/oskari/domain/map/UserDataLayer.java
@@ -9,7 +9,6 @@ import org.json.JSONObject;
 public abstract class UserDataLayer extends JSONLocalizedName {
 
     private long id;
-    private String name; // use name from locale
     private String uuid;
     private String publisher_name;
     private WFSLayerOptions options;
@@ -27,15 +26,7 @@ public abstract class UserDataLayer extends JSONLocalizedName {
     public String getPrefixedId() {
         return getType() + "_" + getId();
     }
-    @Deprecated
-    public String getName() {
-        return name;
-    }
-    @Deprecated
-    public void setName(String name) {
-        this.name = name;
-    }
-    
+
     public String getUuid() {
         return uuid;
     }
